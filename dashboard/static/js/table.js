70513e593b2e2b7028ce02619d8d44296f5af657
eriansha <putra.ivan90@gmail.com>
2018-01-12 13:54:29 +0700

    Added dynamic datatables

--- dashboard/static/js/table.js
+++ dashboard/static/js/table.js
@@ -12,41 +12,17 @@ function tabulate(selection, dataset, columns) {
 	    			});
 	});
 
-	// var tbody = d3.select('#data-fill');
-		// thead = table.append('thead'),
-		// tbody = table.append('tbody'),
-		// tfoot = table.append('tfoot');
-
-	// // append the header row
-	// thead.append('tr')
-	// 	 .selectAll('th')
-	// 	 .data(columns)
-	// 	 .enter()
-	// 	 	.append('th')
-	// 	 	.text(function (column) { return column; });
-
-	// // append the foot row
-	// tfoot.append('tr')
-	// 	 .selectAll('th')
-	// 	 .data(columns)
-	// 	 .enter()
-	// 	 	.append('th')
-	// 	 	.text(function (column) { return column; });
-
 	// create a row for each object in data
 	var rows = selection.selectAll('tr')
 					.data(dictTable)
 					.enter()
-					.append('tr');
-
-	// create a cell in each row for each column
-	var cells = rows.selectAll('td')
-					.data(function(row) {
-							return columns.map(function (column) {
-										return {column: column, value: row[column]};
-									});
-					})
-					.enter()
-					.append('td')	
-					.text(function(d) { return d.value});
+					.append('tr')
+					.html(function(d) {
+						"<td>" + d.kabupaten + "</td>"
+						"<td>" + d.longitude + "</td>"
+						"<td>" + d.latitude + "</td>"
+						"<td>" + d.tahun + "</td>"
+						"<td>" + d.ipm + "</td>"
+						"<td>" + d.cluster + "</td>"
+					});
 }
